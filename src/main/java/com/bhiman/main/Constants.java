337f348e1a595d5a73264a4f65ab2c2eaefad591
mingalkar <mingalkar@gmail.com>
2021-02-22 11:54:31 +0530

    all up date

--- src/main/java/com/bhiman/main/Constants.java
+++ src/main/java/com/bhiman/main/Constants.java
@@ -1,5 +1,35 @@
 package com.bhiman.main;
 
-public class Constants {
+import java.util.ArrayList;
+import java.util.List;
 
+import org.apache.log4j.Logger;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+
+
+import ru.yandex.qatools.ashot.AShot;
+
+public class Constants {
+	public static WebDriver driver;
+	public static WebElement element;
+	public static Actions action;
+	public static Object obj;
+
+	public static List<String> expectedList;
+	public static ArrayList<String> actualList;
+	public static boolean isDisplay;
+	public static String actual;
+	public static String expected;
+
+	public static Logger logger;
+
+	public static AShot ashot;
+
+	
 }
